a3d28720969d4f7717ae230f5b6f036d4dea66fe
narusekoharu <naruse-koharu@techno-concier.co.jp>
2026-04-08 13:05:27 +0900

    コミット

---
+++ src/main/java/com/example/demo/BihinConst.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+// 定数一覧クラス
+public class BihinConst {
+	
+	// 処理可能な結果
+	public static final int RESULT_OK = １;
+	
+	// 処理不可能な結果
+	public static final int RESULT_NG = ０;
+	
+	// 30文字より長い入力だった結果
+	public static final int RESULT_NUM = 3;
+	
+	// 30文字
+	public static final int NAME_LENGTH = 30;
+	
+	// 例外エラーが発生した場合
+	public static final int RESULT_ERROR = 9;
+	
+}
